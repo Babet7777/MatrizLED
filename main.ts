d440beb089e0f9b6840f6836ef39546b7b8a192a
Babet7777 <94986798+Babet7777@users.noreply.github.com>
2021-11-24 19:06:02 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
 basic.forever(function () {
-	
+    if (input.buttonIsPressed(Button.A)) {
+        led.plot(2, 2)
+        basic.pause(300)
+    }
+    if (input.buttonIsPressed(Button.B)) {
+        led.unplot(2, 2)
+        basic.pause(300)
+    }
 })
